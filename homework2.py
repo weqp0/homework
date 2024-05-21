43b9e74fd213bc6c818e72b1bb3e9fd5f132fa5b
weqp0 <168872497+weqp0@users.noreply.github.com>
2024-05-21 21:45:12 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+name = 'Python,'
+number_of_tasks =12
+wasted_time =1.5
+Time_per_task = (wasted_time)/(number_of_tasks)
+print(name, 'всего задач:', number_of_tasks,', затрачено часов:', wasted_time,', среднее время выполнения:', Time_per_task)
